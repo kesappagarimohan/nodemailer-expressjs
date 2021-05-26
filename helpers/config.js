6ab4e0a6e804b66919920c989846225c48caf61c
kesappagarimohan <mohankesappa@gmail.com>
2021-05-26 10:48:28 +0530

    change

--- helpers/config.js
+++ helpers/config.js
@@ -1,20 +1,18 @@
-"use strict";
-
-const dotenv = require("dotenv");
-const assert = require("assert");
+'use strict';
+const dotenv = require('dotenv');
+const assert =  require('assert');
 
 dotenv.config();
 
-const { PORT, HOST, HOST_URL, EMAIL_SERVER, PASSWORD } = process.env;
+const {PORT, HOST, HOST_URL, EMAIL_SERVER, PASSWORD} = process.env;
 
-assert(PORT, "port is required");
-assert(HOST, "Host is required");
-assert(HOST_URL, "Host_url is required");
+assert(PORT, 'Port is required');
+assert(HOST, 'Host is Required');
 
 module.exports = {
-  port: PORT,
-  host: HOST,
-  url: HOST_URL,
-  emailserver: EMAIL_SERVER,
-  password: PASSWORD,
-};
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    emailserver: EMAIL_SERVER,
+    password: PASSWORD
+}
